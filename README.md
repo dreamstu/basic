e7c1fa0c2a60f5a6969f064c7c176614798af4cf
dreamstu <cjxsgn@163.com>
2014-07-15 18:34:12 +0800

    build

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
-# base
+# basic
+
+basic 是一个基础组件，提供一些常用功能,包括但不限于Json、cookie的快捷支持。
 
 ---
 
-[![Build Status](https://travis-ci.org/dreamstu/base.svg?branch=master)](https://travis-ci.org/dreamstu/base)
+[![Build Status](https://travis-ci.org/dreamstu/base.svg?branch=master)](https://travis-ci.org/dreamstu/basic)
 
-A nice Base module.
+一套方便灵活的高可用的前端组合框架.
 
 
 ---
@@ -21,6 +23,430 @@ seajs.use('index', function(base) {
 ```
 
 ## Api
+---
 
-具体函数及注意事项请参阅演示页面
+### isEmpty `object.isEmpty(obj)`	*Object*
+在需要判断某一对象是否为空（指的是为空字符串或者null）时，使用 `object.isEmpty(obj)` 函数，该函数返回一个*Boolean*。
+
+````javascript
+seajs.use(['jquery','index'], function($,base) {
+	$("#isEmptyBtn").on('click',function(){
+		var obj = prompt("请输入点什么吧！","null");
+		$("#isEmpty").html(base.isEmpty(obj)+"");
+	});
+});
+````
+
+````html
+<span>
+	<button id="isEmptyBtn">点击我进行测试</button>
+	执行后结果：
+	<span id="isEmpty"></span>
+</span>
+````
+
+
+### isUndefined `object.isUndefined(obj)`	*Object*
+判断变量的值是否是 undefined，当 o 的值是 undefined 时返回 true *Boolean*。
+
+````javascript
+seajs.use(['jquery','index'], function($,base) {
+	$("#isUndefinedBtn").on('click',function(){
+		var obj = prompt("请输入点什么吧！","undefined");
+		$("#isUndefined").html(base.isUndefined(obj)+"");
+	});
+});
+````
+
+````html
+<span>
+	<button id="isUndefinedBtn">点击我进行测试</button>
+	执行后结果：
+	<span id="isUndefined"></span>
+</span>
+````
+
+
+### isNull `object.isNull(obj)`	*Object*
+判断变量的值是否是 null，当 o 的值是 null 时返回 true *Boolean*。
+
+````javascript
+seajs.use(['jquery','index'], function($,base) {
+	$("#isNullBtn").on('click',function(){
+		$("#isNull").html(base.isNull(null)+"|"+base.isNull("null"));
+	});
+});
+````
+````html
+<span>
+	<button id="isNullBtn">点击我进行测试</button>
+	执行后结果：
+	<span id="isNull"></span>
+</span>
+````
+
+### isNumber `object.isNumber(obj)`	*Object*
+判断变量的类型是否是 Number，当 o 的类型是 number 时返回 true *Boolean*。
+--
+   * @param {Mixed} o 传入被检测变量的名称
+````javascript
+seajs.use(['jquery','index'], function($,base) {
+	$("#isNumberBtn").on('click',function(){
+		$("#isNumber").html(base.isNumber(123)+"|"+base.isNumber("123"));
+	});
+});
+````
+````html
+<span>
+	<button id="isNumberBtn">点击我进行测试</button>
+	执行后结果：
+	<span id="isNumber"></span>
+</span>
+````
+
+### isBoolean|isString|isObject|isArray|isArguments|isFunction	*Object*
+该函数返回一个*Boolean*。
+
+
+
+### $typeof `object.$typeof(obj)`	*Object*
+判断变量的类型是否是什么 ，返回变量的类型，如果不识别则返回 other *String*。
+--
+     * @param {Mixed} o 传入被检测变量的名称
+````javascript
+seajs.use(['jquery','index'], function($,base) {
+	$("#typeofBtn").on('click',function(){
+		var obj = prompt("请输入点什么吧！","null");
+		$("#typeof").html(base.$typeof(obj)+"|"+base.$typeof(123)+"|"+base.$typeof(true)+"|"+base.$typeof(function(){}));
+	});
+});
+````
+````html
+<span>
+	<button id="typeofBtn">点击我进行测试</button>
+	执行后结果：
+	<span id="typeof"></span>
+</span>
+````
+
+### isBoolean|isString|isObject|isArray|isArguments|isFunction	*Object*
+该函数返回一个*Boolean*。
+
+
+### formatParams `object.formatParams(heads,params)`	*Array*,*Array*。
+在需要序列化提交参数(自动将参数中的空字符串转换成null，
+并且将提交参数转化成json对象)时，
+使用 `object.formatParams(heads,params)` 函数，该函数返回一个*Json*。
+
+````javascript
+seajs.use(['jquery','index'], function($,base) {
+	$("#formatParamsBtn").on('click',function(){
+		var heads = new Array("id","name","sex","age","IDcard");
+		var datas = new Array(1,"小白","男",null,"");
+		var json = base.formatParams(heads,datas);
+		console.log(json);
+		$("#formatParams").html(json);
+	});
+});
+````
+
+````html
+<span>
+	<button id="formatParamsBtn">点击我进行测试</button>
+	执行后结果：
+	<span id="formatParams"></span>
+</span>
+````
+
+
+### autoInsertDataByParam `object.autoInsertDataByParam(rst,params)`	*Json*,*Array*
+在需要根据DOM节点ID自动将服务器返回的json数据插入页面DOM中时，使用 `object.autoInsertDataByParam(rst,params)` 函数。
+
+````javascript
+seajs.use(['jquery','index'], function($,base) {
+	$("#autoInsertDataByParamBtn").on('click',function(){
+		//准备的模拟服务器返回的json数据
+		var rst = $.parseJSON('{"msg":"","state":0,"result":{"len":1,"head":["id","name","age"],"data":[[1,"小白","22"]]}}');
+		var params = new Array("id","name","age");
+		base.autoInsertDataByParam(rst,params);
+	});
+});
+````
+
+````html
+<span>
+	<button id="autoInsertDataByParamBtn">点击我进行测试</button>
+	<br/><br/>
+	执行后结果：
+	<br/><br/>
+	id：<input id="id" type="text"/>
+	name：<input id="name" type="text"/>
+	age：<input id="age" type="text"/>
+</span>
+````
+
+### getValueByHead `object.getValueByHead(rst,head)`	*Json*,*String*
+在需要解析服务器返回的JSON对象数据，获取某一head头所对应的data数据时，
+使用 `object.getValueByHead(rst,head)` 函数，该函数返回一个*String*。
+
+````javascript
+seajs.use(['jquery','index'], function($,base) {
+	$("#getValueByHeadBtn").on('click',function(){
+		//准备的模拟服务器返回的json数据
+		var rst = $.parseJSON('{"msg":"","state":0,"result":{"len":1,"head":["id","name","age"],"data":[[1,"小白","22"]]}}');
+		alert(base.getValueByHead(rst,"id"));
+		alert(base.getValueByHead(rst,"name"));
+		alert(base.getValueByHead(rst,"age"));
+	});
+});
+````
+
+````html
+<span>
+	<button id="getValueByHeadBtn">点击我进行测试</button>
+	<br/><br/>
+	执行后结果：【请点击上边的按钮，在乎一下弹出框中的值好吗。。。】
+</span>
+````
+
+### createJsonData `object.createJsonData(head,data)`	*Array*,*Array*
+在需要创建json对象，生成GsonHeadOneRow时，使用 `object.createJsonData(head,data)` 函数，该函数多用于提交数据时生成json数据对象，该函数返回一个*Json*。
+
+````javascript
+seajs.use(['jquery','index'], function($,base) {
+	$("#createJsonDataBtn").on('click',function(){
+		//准备的模拟服务器返回的json数据
+		var head = Array("name","age","sex","id");
+		var data = Array("发哥",21,"男",100);
+		var json = base.createJsonData(head,data);
+		console.log(json);
+	});
+});
+````
+
+````html
+<span>
+	<button id="createJsonDataBtn">点击我进行测试</button>
+	执行后结果：请打开控制台查看log
+</span>
+````
+
+
+### goPage `object.goPage(url)`	*String*
+在需要重定向页面时，使用 `object.goPage(url)` 函数。
+
+````javascript
+seajs.use(['jquery','index'], function($,base) {
+	$("#goPageBtn").on('click',function(){
+		//打开网址：http://www.dreamstu.com/
+		base.goPage("http://www.dreamstu.com/");
+	});
+});
+````
+
+````html
+<span>
+	<button id="goPageBtn">点击我进行测试</button>
+	执行后结果：你懂的、、、
+</span>
+````
+
+
+### back `object.back()`
+在需要后退页面时，使用 `object.back()` 函数。
+
+````javascript
+seajs.use(['jquery','index'], function($,base) {
+	$("#backBtn").on('click',function(){
+		//后退
+		base.back();
+	});
+});
+````
+
+````html
+<span>
+	<button id="backBtn">点击我进行测试</button>
+	执行后结果：你懂的、、、
+</span>
+````
+
+
+### reload `object.reload()`
+在需要重载页面时，使用 `object.reload()` 函数。
+
+````javascript
+seajs.use(['jquery','index'], function($,base) {
+	$("#reloadBtn").on('click',function(){
+		//重载页面
+		base.reload();
+	});
+});
+````
+
+````html
+<span>
+	<button id="reloadBtn">点击我进行测试</button>
+	执行后结果：你懂的、、、
+</span>
+````
+
+### getCurrUrl `object.getCurrUrl()`
+在需要获取当前页面url地址时，使用 `object.getCurrUrl()` 函数，该函数返回一个*String*。
+
+````javascript
+seajs.use(['jquery','index'], function($,base) {
+	$("#getCurrUrlBtn").on('click',function(){
+		//重载页面
+		$("#getCurrUrl").html(base.getCurrUrl());
+	});
+});
+````
+
+````html
+<span>
+	<button id="getCurrUrlBtn">点击我进行测试</button>
+	执行后结果：<span id="getCurrUrl"></span>
+</span>
+````
+
+
+### getUrlParam `object.getUrlParam(name)` *String*
+在需要获取url参数时，使用`object.getUrlParam(name)` 函数，该函数返回一个*String*。
+
+````javascript
+seajs.use(['jquery','index'], function($,base) {
+	//请自己给当前地址加上请求参数	?id=100&name=小白 
+	var id = base.getUrlParam("id");
+	var name = base.getUrlParam("name");
+	$("#getUrlParam").html("ID："+id+"	Name:"+name);
+});
+````
+
+````html
+执行后结果：
+	<span id="getUrlParam"></span>
+````
+
+### random(min, max) *Number|Number*
+生成随机数的方法
+
+>@param {Number} min 生成随机数的最小值
+>@param {Number} max 生成随机数的最大值
+>@return {Number} 返回生成的随机数
+
+---
+
+
+### clone(o) *Object*
+克隆一个对象
+
+>@param {Object} o 要克隆的对象
+>@return {Object} 返回通过克隆创建的对象
+
+````js
+	var objA = {name: "Kinvix"};
+	// 克隆一个 objA 对象，并存入 objB 中。
+	var objB = clone(objA);
+````
+---
+
+
+
+### $return(result) *Object*
+生成一个返回值是传入的 value 值的函数
+
+>@param {Mixed} value 要返回的值
+>@return {Mixed} 返回一个返回值是 value 的函数
+
+---
+
+
+
+
+### $try() 
+按顺序执行 funcA, funcB, funcC，当中途有一个 func 的执行结果返回 true 则不再往下执行，并返回成功执行的 func 的返回值；
+
+
+````js
+$try(funcA, funcB, funcC....);
+````
+
+---
+
+
+
+
+### now()
+获取当前时间的函数
+
+---
+
+
+
+
+### timedChunk(items, process, context, isShift, callback)
+通用分时处理函数
+
+---
+
+
+### getLength(obj)
+获取对象自身具有的属性和方法的数量
+
+>@param {Object} obj 要获取的对象
+
+>@return {Number} 返回对象自身具有属性和方法的数量
+
+---
+
+
+### emptyFunc()
+一个空函数函数
+
+>@return {Number} 返回一个空函数
+
+---
+
+
+
+### pass(func, var_args)
+给函数传入参数并执行
+
+>@param {Mixed} args 参数列表
+
+>@return {Mixed} 返回函数执行结果
+
+示例：
+
+````js
+	// 将"a"、"b"两个字符串传入funcA函数并执行
+    funcA.pass("a","b");
+````
+
+---
+
+
+
+
+### bind(func, context, var_args)
+将一个函数绑定给一个对象作方法，返回的函数将总被传入{@code obj} as {@code this}
+
+>@param {Function} func 要绑定的函数
+
+>@param {Object} contextObj 要绑定的对象
+
+>@param {Mixed} args 参数列表，长度任意
+
+>@return {Function} 返回一个被绑定this上下文对象的函数
+
+
+示例：
+
+````js
+	funcB = bind(funcA, obj, a, b)
+    funcB(c, d) // 相当于执行 funcA.call(obj, a, b, c, d)
+````
+
+---
 
--- index.js
+++ index.js
@@ -1,3 +1,517 @@
-define(function(require, exports, module) {
-	module.exports = require("./src/base");
-});
+	var $ = require("jquery");
+	var JSON = require("jsons");
+	var Log = require("qs-console")
+
+  	var base = {};
+
+  	/**
+	 * 判断当前obj是否为空
+	 * 
+	 * @method isEmpty
+	 * @param {Object} obj 需要验证的对象
+	 * @return {Boolean} obj是否为空,返回true则为空，返回false则不为空
+	 */
+	base.isEmpty = function(obj) {
+		return (!(Boolean($.trim(obj) || obj === 0)) || obj == "null" || obj == "undefined");
+	};
+
+	/**
+     * 判断变量的值是否是 undefined
+     * Determines whether or not the provided object is undefined
+     * 
+     * @method isUndefined
+     * @memberOf Jx.prototype
+     * 
+     * @param {Mixed} o 传入被检测变量的名称
+     * @return {Boolean} 当 o 的值是 undefined 时返回 true
+     */
+    base.isUndefined = function(o) {
+        return typeof(o) === "undefined";
+    };
+        
+    /**
+     * 判断变量的值是否是 null
+     * Determines whether or not the provided object is null
+     * 
+     * @method isNull
+     * @memberOf Jx.prototype
+     * 
+     * @param {Mixed} o 传入被检测变量的名称
+     * @return {Boolean} 当 o 的值是 null 时返回 true
+     */
+    base.isNull = function(o) {
+        return o === null;
+    };
+    
+    /**
+     * 判断变量的类型是否是 Number
+     * Determines whether or not the provided object is a number
+     * 
+     * @memberOf Jx.prototype
+     * @name isNumber
+     * @function
+     * @param {Mixed} o 传入被检测变量的名称
+     * @return {Boolean} 当 o 的类型是 number 时返回 true
+     */
+    base.isNumber = function(o) {
+        return (o === 0 || o) && o.constructor === Number;
+    };
+    
+    /**
+     * 判断变量的类型是否是 Boolean
+     * Determines whether or not the provided object is a boolean
+     * 
+     * 
+     * @method isBoolean
+     * @memberOf Jx.prototype
+     * 
+     * @static
+     * @param {Mixed} o 传入被检测变量的名称
+     * @return {Boolean} 当 o 的类型是 boolean 时返回 true
+     */
+    base.isBoolean = function(o) {
+        return (o === false || o) && (o.constructor === Boolean);
+    };
+    
+    /**
+     * 判断变量的类型是否是 String
+     * Determines whether or not the provided object is a string
+     * 
+     * 
+     * @method isString
+     * @memberOf Jx.prototype
+     * 
+     * @static
+     * @param {Mixed} o 传入被检测变量的名称
+     * @return {Boolean} 当 o 的类型是 string 时返回 true
+     */
+    base.isString = function(o) {
+        return (o === "" || o) && (o.constructor === String);
+    };
+    
+    /**
+     * 判断变量的类型是否是 Object
+     * Determines whether or not the provided object is a object
+     * 
+     * 
+     * @method isObject
+     * @memberOf Jx.prototype
+     * 
+     * @param {Mixed} o 传入被检测变量的名称
+     * @return {Boolean} 当 o 的类型是 object 时返回 true
+     */
+    base.isObject = function(o) {
+        return o && (o.constructor === Object || Object.prototype.toString.call(o) === "[object Object]");
+    };
+    
+    /**
+     * 判断变量的类型是否是 Array
+     * Determines whether or not the provided object is a array
+     * 
+     * 
+     * @method isArray
+     * @memberOf Jx.prototype
+     * 
+     * @param {Mixed} o 传入被检测变量的名称
+     * @return {Boolean} 当 o 的类型是 array 时返回 true
+     */
+    base.isArray = function(o) {
+        return o && (o.constructor === Array || Object.prototype.toString.call(o) === "[object Array]");
+    };
+    
+    /**
+     * 判断变量的类型是否是 Arguments
+     * Determines whether or not the provided object is a arguments
+     * 
+     * 
+     * @method isArguments
+     * @memberOf Jx.prototype
+     * 
+     * @param {Mixed} o 传入被检测变量的名称
+     * @return {Boolean} 当 o 的类型是 arguments 时返回 true
+     */
+    base.isArguments = function(o) {
+        return o && o.callee && isNumber(o.length) ? true : false;
+    };
+    
+    /**
+     * 判断变量的类型是否是 Function
+     * Determines whether or not the provided object is a function
+     * 
+     * 
+     * @method isFunction
+     * @memberOf Jx.prototype
+     * 
+     * @param {Mixed} o 传入被检测变量的名称
+     * @return {Boolean} 当 o 的类型是 function 时返回 true
+     */
+    base.isFunction = function(o) {
+        return o && (o.constructor === Function);
+    };
+
+	/**
+     * 判断变量类型的方法
+     * Determines the type of object
+     * 
+     * 
+     * @method $typeof
+     * @memberOf Jx.prototype
+     * 
+     * @param {Mixed} o 传入被检测变量的名称
+     * @return {String} 返回变量的类型，如果不识别则返回 other
+     */
+    base.$typeof = function(o) {
+        if(base.isUndefined(o)){
+            return "undefined";
+        }else if(base.isNull(o)){
+            return "null";
+        }else if(base.isNumber(o)){
+            return "number";
+        }else if(base.isBoolean(o)){
+            return "boolean";
+        }else if(base.isString(o)){
+            return "string";
+        }else if(base.isObject(o)){
+            return "object";
+        }else if(base.isArray(o)){
+            return "array";
+        }else if(base.isArguments(o)){
+            return "arguments";
+        }else if(base.isFunction(o)){
+            return "function";
+        }else{
+            return "other";
+        }
+        
+    };
+
+	/**
+	 * 序列化提交参数
+	 * 
+	 * @method formatParams
+	 * @param {Array} heads 需要序列化的head头
+	 * @param {Array} params 需要序列化的head头对应的data
+	 * @return {Json} json字符串，可直接提交至服务器
+	 */
+	base.formatParams = function(heads,params){
+		var jsonObj = new Object();
+		jsonObj.head= heads;
+		$.each(params,function(index,value){
+			params[index] = base.isNull(value)==true?null:value;
+		});
+		jsonObj.data=params;
+		return JSON.toJsonString(jsonObj);
+	};
+
+	/**
+	 * 去掉字符串左右两边的空格
+	 * 
+	 * @method trimStr
+	 * @param {Object} obj 需要处理的对象
+	 * @return {String} 处理完成后的obj字符串
+	 */
+	base.trimStr = function(obj){  
+	    // 用正则表达式将前后空格  
+	    // 用空字符串替代。  
+	    return obj.replace(/(^\s*)|(\s*$)/g, "");  
+	};
+
+	/**
+	 * 根据DOM节点ID自动将服务器返回的json数据插入页面DOM中
+	 * 
+	 * @method autoInsertDataByParam
+	 * @param {Json} rst 服务器返回的JSON对象数据
+	 * @param {Array} params 页面所需的数据head头，该head头应该与页面中DOM元素ID保持一致
+	 * @see 解析返回的JSON格式数据
+	 */
+	base.autoInsertDataByParam = function(rst, params) {
+		$.each(params, function(index, perParam) {
+			var str = base.getValueByHead(rst, perParam);
+			console.info(str);
+			if (!base.isNull(str)) {
+				$("#" + perParam).val(str);
+			};
+		});
+	};
+	/**
+	 * 解析服务器返回的JSON对象数据，获取某一head头所对应的data数据
+	 * 
+	 * @method getValueByHead
+	 * @param {json} rst 服务器返回的JSON对象数据
+	 * @param {String} head 某一需要的head
+	 * @return {String} head头对应的data数据值
+	 */
+	base.getValueByHead = function(rst, head) {
+		if (rst == null) {
+			return rst;
+		} else {
+			if (rst.result.len > 0) {
+				var pIndex = -1;
+				$(rst.result.head).each(function(i, perHead) {
+					if (perHead == head) {
+						pIndex = i;
+					}
+				});
+				if(pIndex!=-1)
+					return rst.result.data[0][pIndex];
+				else
+					return null;
+			}else{
+				return null;
+			}
+		}
+	};
+
+	/**
+	 * 创建json对象，该函数多用于提交数据时，生成GsonHeadOneRow
+	 * 
+	 * @method createJsonData
+	 * @param {Array} head head数组
+	 * @param {Array} data data数组
+	 * @return {json} 生成的json对象 类似于GsonHeadOneRow
+	 */
+	base.createJsonData = function(head, data) {
+		var obj = new Object();
+		obj.head = head;
+		obj.data = data;
+		return obj;
+	};
+
+	/**
+	 * 重定向页面
+	 * 
+	 * @method goPage
+	 * @param {String} url 需要重定向的目标页面路径
+	 */
+	base.goPage = function(url){
+		window.location.href = url;
+	};
+	
+	/**
+	 * 后退页面
+	 * 
+	 * @method back
+	 */
+	base.back = function(){
+		history.back();
+	};
+
+	/**
+	 * 重载页面
+	 * 
+	 * @method reload
+	 */
+	base.reload = function(){
+		window.location.reload(Math.random());
+	};
+	
+	/**
+	 * 获取当前页面url地址
+	 * 
+	 * @return {String} 当前页面url地址
+	 */
+	base.getCurrUrl = function() {
+		return window.location.href;
+	};
+
+	/**
+	 * 获取url参数
+	 * 
+	 * @method getUrlParam
+	 * @param {String} name 参数名
+	 * @return {String} 获取到的结果
+	 */
+	base.getUrlParam = function(name) {
+		var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); // 构造一个含有目标参数的正则表达式对象
+		var r = base.decodingURI(window.location.search).substr(1).match(reg); // 匹配目标参数
+		if (r != null)
+			return r[2];
+		return null; // 返回参数值
+	};
+
+	/**
+     * 生成随机数的方法
+     * 
+     * @method random
+     * 
+     * @param {Number} min 生成随机数的最小值
+     * @param {Number} max 生成随机数的最大值
+     * @return {Number} 返回生成的随机数
+     */
+    base.random = function(min, max){
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    };
+
+    /**
+     * 克隆一个对象
+     * 
+     * @method clone
+     * 
+     * @param {Object} o 要克隆的对象
+     * @return {Object} 返回通过克隆创建的对象
+     * 
+     * @example
+     * Jx().$package(function(J){
+     *     var objA = {name: "Kinvix"};
+     *     // 克隆一个 objA 对象，并存入 objB 中。
+     *     var objB = clone(objA);
+     * };
+     */
+    base.clone = function(o){
+        /**
+         * @ignore
+         */
+        var tempClass = function(){};
+        tempClass.prototype = o;
+        
+        // 返回新克隆的对象
+        return (new tempClass());
+    };
+
+	/**
+     * 生成一个返回值是传入的 value 值的函数
+     * 
+     * @method $return
+     * 
+     * @param {Mixed} value 要返回的值
+     * @return {Mixed} 返回一个返回值是 value 的函数
+     */
+    base.$return = function(result){
+        return isFunction(result) ? result : function(){
+                return result;
+            };
+    };
+
+    /***
+    * 按顺序执行 funcA, funcB, funcC，当中途有一个 func 的执行结果返回 true 则不再往下执行，并返回成功执行的 func 的返回值；
+    * 	$try(funcA, funcB, funcC);
+    */
+    base.$try = function(){
+        var i,
+            l = arguments.length,
+            result;
+            
+        for(i = 0; i < l; i++){
+            try{
+                result = arguments[i]();
+                // 如果上边语句执行成功则执行break跳出循环
+                break;
+            }catch(e){
+                Log.error("C.错误：[" + e.name + "] "+e.message+", " + e.fileName+", 行号:"+e.lineNumber+"; stack:"+typeof e.stack, 2);
+            }
+        }
+        return result;
+    };
+
+    /**
+     * 获取当前时间的函数
+     * 
+     * @method now
+     * 
+     * @example
+     * alert(now());
+     * 
+     */
+    base.now = function(){
+        return +new Date;
+    };
+
+    /**
+     * 通用分时处理函数
+     * 
+     * @method timedChunk
+     * 
+     * 
+     * 
+     * 
+     */
+    base.timedChunk = function(items, process, context, isShift, callback) {
+        var todo = items.concat(), delay = 25;
+        if(isShift){
+            todo = items;
+        }
+ 
+        window.setTimeout(function() {
+            var start = +new Date();
+ 
+            do {
+                process.call(context, todo.shift());
+            } while(todo.length > 0 && (+new Date() - start < 50));
+ 
+            if(todo.length > 0) {
+                window.setTimeout(arguments.callee, delay);
+            } else if(callback) {
+                callback(items);
+            }
+ 
+        }, delay);
+    };
+
+    /**
+     * 获取对象自身具有的属性和方法的数量
+     * 
+     * @method getLength
+     * 
+     * @param {Object} obj 要获取的对象
+     * @return {Number} 返回对象自身具有属性和方法的数量
+     */
+    base.getLength = function(obj) {
+        var p,
+            count = 0;
+        for(p in obj){
+            if(obhasOwnProperty(p)){
+                count++;
+            }
+        }
+        return count;
+    };
+    
+    /**
+     * 一个空函数函数
+     * 
+     */
+    base.emptyFunc = function(){};
+    
+    /**
+     * 给函数传入参数并执行
+     * 
+     * @param {Mixed} args 参数列表
+     * @return {Mixed} 返回函数执行结果
+     * 
+     * @example
+     *     // 将"a"、"b"两个字符串传入funcA函数并执行
+     *     funcA.pass("a","b");
+     * 
+     */
+    base.pass = function(func, var_args) {
+        var slice = Array.prototype.slice;
+        var a = slice.call(arguments, 1);
+        return function(){
+            var context = this;
+            return func.apply(context, a.concat(slice.call(arguments)));
+        };
+    };
+
+    /**
+     * 将一个函数绑定给一个对象作方法，返回的函数将总被传入{@code obj} as {@code this}
+     * 
+     * @param {Function} func 要绑定的函数
+     * @param {Object} contextObj 要绑定的对象
+     * @param {Mixed} args 参数列表，长度任意
+     * @return {Function} 返回一个被绑定this上下文对象的函数
+     * 
+     * @example
+     *   funcB = bind(funcA, obj, a, b)
+     *   funcB(c, d) // 相当于执行 funcA.call(obj, a, b, c, d)
+     */
+    
+    base.bind = function(func, context, var_args) {
+        var slice = Array.prototype.slice;
+        var a = slice.call(arguments, 2);
+        return function(){
+            return func.apply(context, a.concat(slice.call(arguments)));
+        };
+    };
+
+
+  module.exports = base;
